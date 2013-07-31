aacc8d805767a9e93cbe5a7c7111d8d197a6ce97
mactive <mactive@gmail.com>
2013-07-31 23:00:22 +0800

    hover thumbnail

--- wp-content/themes/twentytwelve/content-thumbnail.php
+++ wp-content/themes/twentytwelve/content-thumbnail.php
@@ -14,13 +14,28 @@
 			$id = $post->ID; 
 			$args = array( 'post_type' => 'attachment', 'post_parent'=> $id, 'post_mime_type' => 'image/jpeg' );
 			$myposts = get_posts( $args );
+			// attachment
 			$attachment = $myposts[0];
+			// thumbnail
 			$thumb = wp_get_attachment_image_src( $attachment->ID, 'large' );
+
+
+			// meta
+			$post_meta 	= get_post_meta($post->ID);
+            $customer 	= $post_meta['wpcf-customer'][0];
+            $agency 	= $post_meta['wpcf-agency'][0];
+            $production = $post_meta['wpcf-production'][0];
+
 		?>
 
 
 		<div class="video_thumbnail" style="background-image:url(<?= $thumb[0] ?>);">
-
+			<div class="thunbmail_info">
+				篇名: <?php the_title(); ?><br>
+				客户: <?= $customer; ?><br>
+				代理: <?= $agency; ?><br>
+				制作: <?= $production; ?>
+			</div>
 		</div>
 		
 		
