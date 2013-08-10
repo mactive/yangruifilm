a55e54ea18135984bfb077128f3ae112f31240a4
mactive <mactive@gmail.com>
2013-08-10 23:00:33 +0800

    about me

--- wp-content/themes/twentytwelve/content-page.php
+++ wp-content/themes/twentytwelve/content-page.php
@@ -7,17 +7,37 @@
  * @since Twenty Twelve 1.0
  */
 ?>
+<style type="text/css">
+.wrapper{background: #fff !important;}
+.site-content{width: 90%; padding:3% 5%; font-size: 14px;}
+.entry-content{text-align:justify; font-size: 14px; line-height: 1.2em;}
+</style>
 
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 		<header class="entry-header">
-			<h1 class="entry-title"><?php the_title(); ?></h1>
-		</header>
+<!-- 			<h1 class="entry-title"><?php the_title(); ?></h1>
+ -->		</header>
 
 		<div class="entry-content">
 			<?php the_content(); ?>
 			<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'twentytwelve' ), 'after' => '</div>' ) ); ?>
 		</div><!-- .entry-content -->
-		<footer class="entry-meta">
-			<?php edit_post_link( __( 'Edit', 'twentytwelve' ), '<span class="edit-link">', '</span>' ); ?>
-		</footer><!-- .entry-meta -->
+
 	</article><!-- #post -->
+
+
+
+<script type="text/javascript">
+(function($){
+	$('.more-meta').appendTo('#more-5');
+	$('#more-5').parent().nextAll().hide();
+	$('.more-meta').children('a.more').click(function(){
+		$('#more-5').parent().nextAll().fadeIn();
+		$(this).fadeOut();
+
+		$("html, body").animate({ scrollTop: $(this).position().top-50 }, 1000);
+	});
+
+})(jQuery);
+</script>
+	
--- wp-content/themes/twentytwelve/page.php
+++ wp-content/themes/twentytwelve/page.php
@@ -19,11 +19,9 @@ get_header(); ?>
 
 			<?php while ( have_posts() ) : the_post(); ?>
 				<?php get_template_part( 'content', 'page' ); ?>
-				<?php comments_template( '', true ); ?>
 			<?php endwhile; // end of the loop. ?>
 
 		</div><!-- #content -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
