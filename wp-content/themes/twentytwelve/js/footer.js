720bec1fab2d4e0c49f6f7bbaae14ed5ca528da6
mactive <mactive@gmail.com>
2013-08-20 16:48:41 +0800

    0.5

--- wp-content/themes/twentytwelve/js/footer.js
+++ wp-content/themes/twentytwelve/js/footer.js
@@ -5,7 +5,7 @@
 
 	var win_height = $(window).height();
 	var menu_height = $('#site-navigation').height();
-	var _margin_top = ( win_height - menu_height) * 0.6;
+	var _margin_top = ( win_height - menu_height) * 0.5;
 
 
 	$('#site-navigation').css('margin-top', _margin_top + 'px');
