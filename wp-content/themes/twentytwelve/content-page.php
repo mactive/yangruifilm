689a4a86f2b9976a75576343cc30e54649713f2e
mactive <mactive@gmail.com>
2013-08-21 23:24:35 +0800

    ls

--- wp-content/themes/twentytwelve/content-page.php
+++ wp-content/themes/twentytwelve/content-page.php
@@ -20,13 +20,29 @@
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 		<header class="entry-header">
 <!-- 			<h1 class="entry-title"><?php the_title(); ?></h1>
- -->		</header>
+ -->	</header>
 
 		<div class="entry-content">
-			<?php the_content(); ?>
-			<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'twentytwelve' ), 'after' => '</div>' ) ); ?>
-		</div><!-- .entry-content -->
+			<?php if ($post_slug == 'contact'): ?>
+				<div class="contact-form">
+					<span class="input-title">留下邮箱地址，即可订阅更新的作品信息</span><br>
+					<span class="input-title"> 邮件地址： </span><input type="text" size="30" plaseholder="输入Email地址">	 
+					<span class="input-title"> 姓名：</span> <input type="text" size="10" plaseholder="输入姓名">
+					<input type="submit" value="订阅">
+				</div>
+				<div class="contact-info">
+					<div class="wechat">
+						<img src="<?php bloginfo( 'template_url' ); ?>/img/wechat.png" /> 
+					</div>
+					<div class="info-text">
+						+86 18911072107   /   yangrui@yangruifilms.com</div>
+				</div>
+			<?php else: ?>
+				<?php the_content(); ?>
+			<?php endif; ?>
 
+
+		</div><!-- .entry-content -->
 	</article><!-- #post -->
 
 
@@ -35,25 +51,26 @@
 (function($){
 	// more page
 	<?php if ($post_slug == 'about'): ?>
+	
+		var more_meta = $('<a class="more">more...</a>');
+		more_meta.appendTo('#more-5');
+		$('#more-5').parent().nextAll().hide();
+		$('#more-5').children('a.more').on('click',function(){
+			$('#more-5').parent().nextAll().fadeIn();
+			$(this).fadeOut();
+			$("html, body").animate({ scrollTop: $(this).position().top-10 }, 1000);
+		});
 
-	$('.more-meta').appendTo('#more-5');
-	$('#more-5').parent().nextAll().hide();
-	$('.more-meta').children('a.more').click(function(){
-		$('#more-5').parent().nextAll().fadeIn();
-		$(this).fadeOut();
-
-		$("html, body").animate({ scrollTop: $(this).position().top-50 }, 1000);
-	});
 	<?php endif; ?>
 
 	<?php if ($post_slug == 'schedule'): ?>
+		
 		var table = $('table.calendar');
 		var td_width =  $('table.calendar tbody').children('tr:eq(1)').children('td:eq(0)').width();
 		console.log(td_width);
 		var td_height = parseInt(td_width * 9 / 16 );
 
 		// $('table.calendar tbody').children('tr:gt(0)').children('td').css('height',td_height+'px');
-
 
 	<?php endif; ?>
 
